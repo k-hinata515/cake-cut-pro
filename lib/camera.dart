c72c0c6cb560a658540b5efa4c5b72e70a37536c
aaaa <2220140@ecc.ac.jp>
2024-10-17 11:12:16 +0900

    カメラ１

--- lib/camera.dart
+++ lib/camera.dart
@@ -1,25 +1,39 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
+late File _image;
 final ImagePicker _picker = ImagePicker();
 
-void main() => runApp(MyApp());
+void main() => runApp(Camera());
 
-class MyApp extends StatelessWidget {
+class Camera extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: CameraPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class CameraPage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<CameraPage> {
+
+  Future getImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Text('No image selected.'),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
         child: Icon(Icons.add_a_photo),
       ),
     );
